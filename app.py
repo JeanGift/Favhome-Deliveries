515a23fa3501fc6117a53be317a475f880e870de
JeanGift <jeangift128@gmail.com>
2025-11-18 15:47:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -628,7 +628,7 @@ def ping():
 
 # ----------------------- RUN -----------------------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    print("FavHome Deliveries running...")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
 
 
